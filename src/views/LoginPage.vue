36fd51ae6711a983ead288925107c245920b8061
구혜연 <guhaa03@daum.net>
2025-07-29 15:36:44 +0900

    feat : 로그인 페이지 구현

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="4" md="6">
+        <v-card>
+          <v-card-title class="text-h5 text-center"> 로그인</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="doLogin">
+              <v-text-field
+                label="이메일"
+                v-model="email"
+                type="email"
+                required
+              >
+              </v-text-field>
+
+              <v-text-field
+                label="비밀번호"
+                v-model="password"
+                type="password"
+                required
+              ></v-text-field>
+
+              <v-btn type="submit" color="primary" block>로그인</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  created() {},
+  methods: {
+    // 서버한테 post 요청으로 토큰을 받아와서 로컬스토리지에 저장,
+    async doLogin() {
+      const loginData = {
+        email: this.email,
+        password: this.password,
+      };
+
+      const res = await axios.post(
+        `${process.env.VUE_APP_API_BASE_URL}/member/doLogin`,
+        loginData
+      );
+
+      // console.log("응답 : ", res);
+      const token = res.data.token;
+      localStorage.setItem("token", token);
+      window.location.href = "/";
+    },
+  },
+};
+</script>
