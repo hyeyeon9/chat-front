df0f62d6d1b947be6b42c81cd3dc3a7054c22a7a
구혜연 <guhaa03@daum.net>
2025-08-08 22:34:11 +0900

    chore : @mdi/font install

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,16 @@
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
+import { aliases, mdi } from 'vuetify/iconsets/mdi' // ← 이거 중요
+import '@mdi/font/css/materialdesignicons.css'       // ← 폰트도 불러오기!
 
 export default createVuetify({
   components,
+  icons: {
+    defaultSet: 'mdi',   // 기본 아이콘 세트 설정
+    aliases,             // mdi-alias들 연결
+    sets: {
+      mdi,               // mdi 세트 사용
+    },
+  },
 });
